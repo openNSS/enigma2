570641bba652c801dd667b0505781085c8b3192d
Fender <mirosio2@hotmail.it>
2021-01-20 15:37:58 +0100

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -28,7 +28,7 @@ class About(Screen):
 		cpu = about.getCPUInfoString()
 		AboutText += _("CPU: ") + cpu + "\n"
 		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
-		AboutText += _("Info: www.nonsolosat.net") + "\n"
+		AboutText += _("Info: www.Nonsolosat.net") + "\n"
 
 		# [WanWizard] Removed until we find a reliable way to determine the installation date
 		# AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
